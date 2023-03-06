830779b618d2db1232c68060d422f1797f1cfa3c
Christopher <tchry902@gmail.com>
2023-03-06 13:12:47 +0100

    work at the button

--- todolist/src/Components/Home/index.jsx
+++ todolist/src/Components/Home/index.jsx
@@ -3,10 +3,24 @@ import styles from "./styles.module.scss";
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
-      <header>
+      <header className={styles.headersWrapper}>
         <h3 className={styles.lavoro}>Lavoro</h3>
+
+        <div className={styles.rectanglesWrapper}>
+          <div className={styles.rectangleLeft}>
+            <span className={styles.options}>Predefinito</span>
+            <div className={styles.path}>↓</div>
+          </div>
+
+          <div className={styles.rectangleRigth}>
+            <div className={styles.shape}>+</div>
+            <span className={styles.nuovaSezione}>Nuova sezione</span>
+          </div>
+        </div>
       </header>
+
       <body></body>
+
       <footer></footer>
     </div>
   );
