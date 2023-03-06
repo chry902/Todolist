b1b5fbc086b44ad3fb1ccda1bf5fa1fdd8ba2343
Christopher <tchry902@gmail.com>
2023-03-06 11:56:56 +0100

    create the first component

---
+++ todolist/src/Components/Home/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <header>
+        <h3 className={styles.lavoro}>Lavoro</h3>
+      </header>
+      <body></body>
+      <footer></footer>
+    </div>
+  );
+};
+export default Home;
