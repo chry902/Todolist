49822b7e2a66fb479a3b345fcbd28063a7afe2c6
Christopher <tchry902@gmail.com>
2023-03-07 17:21:39 +0100

    finisch the last task

--- todolist/src/Components/Home/index.jsx
+++ todolist/src/Components/Home/index.jsx
@@ -1,17 +1,20 @@
 import { useState, useId } from "react";
+
 import Todo from "../Todo";
 import styles from "./styles.module.scss";
 
 const Home = ({ todo, setTodo }) => {
   const uniqId = useId();
-  const [active, setActive] = useState(false);
-  const [actModal, setActiveModal] = useState(false);
+  const [active, setActive] = useState(false); //modale option predefinito...
+  const [actModal, setActiveModal] = useState(false); //modale add task
+  const [modalDeletedItem, setModalDeletedItem] = useState(false); //stato per controllo apertura e chiusura modale di conf. elimina task
+  const [callTask, setCallTask] = useState({}); //stato che passa come prop per prendere dsk da eliminare
 
-  const [nameActiviti, setNameActiviti] = useState("");
-  const [descrp, setDescrpt] = useState("");
-  const [time, setTime] = useState("");
-  console.log("props", todo);
+  const [nameActiviti, setNameActiviti] = useState(""); //
+  const [descrp, setDescrpt] = useState(""); // stati per per conservare volori che arrivano dagli input... per creare nuovitask
+  const [time, setTime] = useState(""); //
 
+  // funzione per creare nuoa task
   const addTodo = (e) => {
     e.preventDefault();
     const id = uniqId;
@@ -21,16 +24,54 @@ const Home = ({ todo, setTodo }) => {
       description: descrp,
       time: time,
     };
-    setTodo((prev) => [...todo, newTodo]);
+    setTodo([...todo, newTodo]); //concateno gli elementi task con quella appena creata
     console.log("add todo", todo);
-    setNameActiviti("");
-    setDescrpt("");
-    setTime("");
-    setActiveModal(!actModal);
+
+    setNameActiviti(""); //
+    setDescrpt(""); // imposto gli stati input nuovamente come stringhe vuote per cancellare la task appena creata una volta creata
+    setTime(""); //
+    setActiveModal(!actModal); // cambio valore stato in false per chiudere la modale add task una volta terminata la funzione
+  };
+
+  //funzione per cancellare task
+  const deletedTask = (e) => {
+    e.preventDefault();
+    console.log("TaskPREsa>>>", callTask);
+    setTodo(todo.filter((el) => el !== callTask)); //prendo stato contenente le task e gli dico di filtrare tutte le task tranne quella che gli passo
+    setModalDeletedItem(!modalDeletedItem); // cambio valore stato in false per chiudere la modale add task una volta terminata la funzione
   };
 
   return (
     <div className={styles.homeContainer}>
+      {/***************************  modale conferma eliminazione task */}
+      <div
+        className={`${styles.optionDeletedModale} ${
+          modalDeletedItem === true ? styles.showDeletedModal : ""
+        }`}
+      >
+        <div className={styles.deletModal}>
+          <div className={styles.eliminaAttivita}>Elimina attività</div>
+          <div className={styles.unaVoltaEliminata}>
+            Una volta eliminita, l’attività sarà persa
+          </div>
+          <div className={styles.btnWrapper}>
+            <div
+              className={styles.btnAnnulla}
+              onClick={() => setModalDeletedItem(!modalDeletedItem)}
+            >
+              Annulla
+            </div>
+            <button
+              className={styles.btnConferma}
+              onClick={(e) => deletedTask(e)} // call funzione conferma cancellazione gli passo (e) evento per annullare gli eeventi di defoult del bottone
+            >
+              conferma
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/***************************  modale input per agg task  */}
       <div
         className={`${styles.modale} ${
           actModal === true ? styles.activeMoidale : ""
@@ -45,7 +86,6 @@ const Home = ({ todo, setTodo }) => {
             X
           </div>
         </div>
-
         <div className={styles.inputWrapper}>
           <form className={styles.form} onSubmit={addTodo}>
             <label className={styles.label}>Nome attivita</label>
@@ -88,6 +128,8 @@ const Home = ({ todo, setTodo }) => {
           </form>
         </div>
       </div>
+
+      {/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
       <header className={styles.headersWrapper}>
         <h3 className={styles.lavoro}>Lavoro</h3>
 
@@ -125,10 +167,20 @@ const Home = ({ todo, setTodo }) => {
         </div>
       </header>
 
+      {/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
       <main className={styles.mainWrapper}>
-        {todo && todo.map((item) => <Todo item={item} key={item.id} />)}
+        {todo &&
+          todo.map((item) => (
+            <Todo
+              item={item} //
+              key={item.id} // utilizzo metodo map passandogli un componente task per stampare gli elementi (task) conteneti dentro il mio stato
+              setCallTask={setCallTask} //
+              setModalDeletedItem={setModalDeletedItem}
+              modalDeletedItem={modalDeletedItem}
+            />
+          ))}
       </main>
-
+      {/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
       <footer className={styles.footerWrapper}>
         <div
           className={styles.addTaskButton}
--- todolist/src/Components/Todo/index.jsx
+++ todolist/src/Components/Todo/index.jsx
@@ -1,18 +1,72 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
-const Todo = ({ item }) => {
+const Todo = ({ item, setCallTask, setModalDeletedItem, modalDeletedItem }) => {
   const nome = item.name || "name";
   const descript = item.description || "descrpt";
   const time = item.time || "9:00";
 
+  const [actionTask, setActionTasck] = useState(false); //azione per cambiare i cerchi rossi in check
+
+  //funzione che mi apre la modale per la conferma dell'eliminazione task
+  const openModal = (value) => {
+    setModalDeletedItem(!modalDeletedItem); //controllo per aprire modale
+    console.log("value", value);
+    setCallTask(value); // stato che prende la task da eliminare
+  };
   return (
     <div className={styles.taskContain}>
       <div className={styles.taskwrapper}>
-        <div className={styles.shape}></div>
-        <div className={styles.nomeTask}>{nome}</div>
+        {actionTask === true ? (
+          <div
+            className={styles.shapeGreen}
+            onClick={() => setActionTasck(!actionTask)} //avvio funzione di call per cambiare stato... e cambiare i cerchi rossi ed sbarrare testo
+          >
+            ✓
+          </div>
+        ) : (
+          <div
+            className={styles.shapeRed}
+            onClick={() => setActionTasck(!actionTask)}
+          ></div>
+        )}
+
+        <div
+          className={`${styles.nomeTask} ${
+            actionTask === true ? styles.lineThrough : ""
+          }`}
+        >
+          {nome}
+        </div>
       </div>
-      <div className={styles.descTask}>{descript}</div>
-      <div className={styles.timeTask}>{time}</div>
+      <div className={styles.descTaskWrapper}>
+        <div
+          className={`${styles.descTask} ${
+            actionTask === true ? styles.lineThrough : ""
+          }`}
+        >
+          {descript}
+        </div>
+        <div className={styles.optionDeletedTask}>⋯</div>
+        <div className={styles.smallModale}>
+          <div className={styles.modificaAttivita}>
+            <span>Modifica attivita</span>
+          </div>
+          <div
+            className={styles.eliminaAttivita}
+            onClick={() => openModal(item)} //call della funzione avvia modale per conferma elim. e passare la task da eliminare
+          >
+            <span>Elimina attivita</span>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${styles.timeTask} ${
+          actionTask === true ? styles.lineThrough : ""
+        }`}
+      >
+        {time}
+      </div>
     </div>
   );
 };
